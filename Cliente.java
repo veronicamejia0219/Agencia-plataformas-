0fa9abb18bbf6b1c64fd51b0e672163afa58a23d
ProfeAlexgi <135893962+ProfeAlexgi@users.noreply.github.com>
2026-03-12 22:18:39 -0500

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente {
+    
+  
+
+    protected String nombre;
+    protected String apellido;
+    protected String telefono;
+    protected String cedula;
+    
+    
+    public Cliente(String nombre, String apellido, String telefono,String cedula ) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.cedula=cedula;
+    }
+
+    public String getNombre(){return nombre;}
+    public String getApellido(){return apellido;}
+    public String getTelefono(){return telefono;}
+    public String getCedula(){return cedula;}
+     
+     
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + getNombreCompleto());
+        System.out.println("Telefono: " + telefono);
+    }
