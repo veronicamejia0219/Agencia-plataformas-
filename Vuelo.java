eaf12e6f43a136e84e1c577fb84d31d74b5586e9
vale <valehl0816@gmail.com>
2026-03-17 14:26:45 -0500

    Añadi clase Vuelo con constructor y método obtenerVuelo

--- Vuelo.java
+++ Vuelo.java
@@ -1,4 +1,21 @@
 public class Vuelo{
+    int idVuelos;
+    String destino;
+    LocalTime horaLocalTime;
+    float precio;
 
-  
+    public Vuelo(int idVuelos, String destino, LocalTime horaLocalTime, float precio) {
+        this.idVuelos = idVuelos;
+        this.destino = destino;
+        this.horaLocalTime = horaLocalTime;
+        this.precio = precio;
+    }
+
+    void obtenerVuelo(){
+        System.out.println("----- INFORMACIÓN DEL VUELO -----");
+        System.out.println("Informacion del vuelo - ID: " + idVuelos + 
+        " Destino: " + destino + 
+        " Hora: " + horaLocalTime + 
+        " Precio: $" + precio);
+    }
 }
