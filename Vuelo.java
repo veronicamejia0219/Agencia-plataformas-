cb4a8fed8637ddeee6398b5520459d31a7f9c313
Jhoset Alexander Hernandez Martinez <jhoseta.hernandezm@autonoma.edu.co>
2026-03-17 19:41:59 -0500

    Añadi un metodo para buscar el vuelo por su ID

--- Vuelo.java
+++ Vuelo.java
@@ -1,4 +1,30 @@
 public class Vuelo{
+    int idVuelos;
+    String destino;
+    LocalTime horaLocalTime;
+    float precio;
 
-  
+    public Vuelo(int idVuelos, String destino, LocalTime horaLocalTime, float precio) {
+        this.idVuelos = idVuelos;
+        this.destino = destino;
+        this.horaLocalTime = horaLocalTime;
+        this.precio = precio;
+    }
+
+    void obtenerVuelo(){
+        System.out.println("----- INFORMACIÓN DEL VUELO -----");
+        System.out.println("Informacion del vuelo - ID: " + idVuelos + 
+        " Destino: " + destino + 
+        " Hora: " + horaLocalTime + 
+        " Precio: $" + precio);
+    }
+    
+        void buscarVueloPorId(int idBusqueda) {
+        if (this.idVuelos == idBusqueda) {
+            System.out.println("¡Vuelo encontrado!");
+            obtenerVuelo(); // Llama a tu otro método para mostrar los datos
+        } else {
+            System.out.println("El ID " + idBusqueda + " no coincide con este vuelo.");
+        }
+    }
 }
